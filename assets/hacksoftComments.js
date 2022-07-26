1f8bd1c6b754ed80d091dd50b52138178b356786
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-07-26 14:44:12 +0300

    added hacksoftComments data to create netlify Api

---
+++ assets/hacksoftComments.js
@@ -0,0 +1,52 @@
+const comments = [
+  {
+    id: 1,
+    comment:
+      'Despite our total project numbers only increasing by 2% compared to last month, the 58 projects we are working on contain a significant increase in deliverables. Bla bla...',
+    createdAt: '20 minutes ago',
+    likes: 4,
+    image: '',
+    username: 'Daniel Goshev',
+    position: 'Software Developer, HackSoft',
+  },
+  {
+    id: 2,
+    comment:
+      'Despite our total project numbers only increasing by 2% compared to last month, the 58 projects we are working on contain a significant increase in deliverables.',
+    createdAt: '3d ago',
+    likes: 4,
+    image: '',
+    username: 'Daniel Goshev',
+    position: 'Software Developer, HackSoft',
+  },
+  {
+    id: 3,
+    comment:
+      'Despite our total project numbers only increasing by 2% compared to last month, the 58 projects we are working on contain a significant increase in deliverables.',
+    createdAt: '3d ago',
+    likes: 0,
+    image: '',
+    username: 'Daniel Goshev',
+    position: 'Software Developer, HackSoft',
+  },
+  {
+    id: 4,
+    comment:
+      'Despite our total project numbers only increasing by 2% compared to last month, the 58 projects we are working on contain a significant increase in deliverables. Bla bla...',
+    createdAt: '3d ago',
+    likes: 4,
+    image: '',
+    username: 'Daniel Goshev',
+    position: 'Software Developer, HackSoft',
+  },
+  {
+    id: 5,
+    comment:
+      'Despite our total project numbers only increasing by 2% compared to last month, the 58 projects we are working on contain a significant increase in deliverables. Bla bla...',
+    createdAt: '3d ago',
+    likes: 4,
+    image: '',
+    username: 'Daniel Goshev',
+    position: 'Software Developer, HackSoft',
+  },
+];
---
+++ functions/hackSoftComments.js
@@ -0,0 +1,13 @@
+const comments = require('../assets/hacksoft-comments');
+//we see our flights on http://localhost:8888/api/hacksoftComments
+//API URL after deployment on Netlify: https://sweta-serverless-functions.netlify.app/api/hacksoftComments
+
+exports.handler = async (event, context) => {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify(comments),
+  };
+};
